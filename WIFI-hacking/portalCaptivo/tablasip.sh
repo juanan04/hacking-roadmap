d8b91bf2e68d9dc30100438100c1b1f34fc42e68
Juan Antonio <130975869+juanan04@users.noreply.github.com>
2025-06-05 17:19:56 +0200

    Added .conf and sh files

---
+++ WIFI-hacking/portalCaptivo/tablasip.sh
@@ -0,0 +1,14 @@
+let interface=
+
+echo 1 > /proc/sys/net/ipv4/ip_forward
+iptables --flush
+iptables --table nat --flush
+iptables --delete-chain
+iptables --table nat --delete-chain
+iptables -P FORWARD ACCEPT
+
+dnsmasq -C dnsmasq.conf
+hostapd hostapd.conf -B
+service apache2 start
+
+ifconfig interface 10.0.0.1 netmask 255.255.255.0
